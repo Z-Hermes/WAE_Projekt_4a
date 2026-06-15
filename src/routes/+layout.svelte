05e5e848a7abd4d90d543eb1328a53fd54cccdd5
Z-Hermes <zallahermez@gmail.com>
2026-06-15 15:02:30 +0200

    page.server.js file fixed

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,18 +1,31 @@
-
 <script>
-	let { children } = $props();
+	let { data, children } = $props();
 </script>
 
-{@render children()}
+<header class="bg-zinc-950 border-b border-zinc-800">
+	<div class="max-w-6xl mx-auto flex justify-between items-center p-4">
+		<a
+			href="/"
+			class="font-bold text-2xl bg-gradient-to-r from-purple-500 via-pink-500 to-orange-400 bg-clip-text text-transparent"
+		>
+			InstaClone
+		</a>
 
+		<nav class="flex gap-4 text-white">
+			<a href="/">Home</a>
 
-<header class="bg-0d0d0d text-white p-4 flex items-center justify-between">
-    <img src="" alt="">
-    <div>
-        <a href="/">Home</a>
-        <a href="/profile">Profile</a>
-        <a href="/login">Login</a>
-        <a href="/register">Register</a>
-        <a href="/logout">Logout</a>
-    </div>
+			{#if data.user}
+				<a href="/admin">Dashboard</a>
+				<a href="/profile/{data.user.id}">Profile</a>
+				<a href="/logout">Logout</a>
+			{:else}
+				<a href="/login">Login</a>
+				<a href="/register">Register</a>
+			{/if}
+		</nav>
+	</div>
 </header>
+
+<main class="min-h-screen bg-zinc-950 text-white">
+	{@render children()}
+</main>
--- src/routes/+page.server.js
+++ src/routes/+page.server.js
@@ -1,11 +1,26 @@
-import pool from "$lib/server/database.js";
+import pool from '$lib/server/database.js';
 
 export async function load() {
+	const [posts] = await pool.execute(`
+        SELECT
+            images.*,
+            users.username
+        FROM images
+        JOIN users
+        ON users.id = images.author_id
+        ORDER BY created_at DESC
+        LIMIT 25
+    `);
 
-    const [rows] = await pool.execute('SELECT description, id , image_url as url, created_at, author_id from images');
+	const [topPosts] = await pool.execute(`
+        SELECT *
+        FROM images
+        ORDER BY votes DESC
+        LIMIT 3
+    `);
 
-    return {
-        pageTitle: "list of posts",
-        posts: rows
-    }
+	return {
+		posts,
+		topPosts
+	};
 }
